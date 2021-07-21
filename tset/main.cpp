dec7c5896bd46423134be6b7b2e004c70ee1d225
wangyw6716 <671682551@qq.com>
2021-07-21 17:08:16 +0800

    修改AI

--- tset/main.cpp
+++ tset/main.cpp
@@ -1,9 +1,9 @@
 #include"Hfolder.h"
 #pragma comment(lib,"winmm.lib")//加载库---音乐
 
-void setStep1(int x, int y, int type);
+//void setStep1(int x, int y, int type);
 void AI();
-void search1(int x, int y);
+//void search1(int x, int y);
 //遍历棋子个数
 void number()
 {
@@ -63,7 +63,6 @@ void hihe()
 	if (ixd == 1)
 	{
 		//zouzi(1);  //调用走子函数
-		//AI(1);
 		AI();
 	}
 	else if (ixd == 0)
@@ -313,7 +312,6 @@ void zouzi(int he)
 				{
 					//MessageBox(NULL, "不是你的回合", "提示", MB_OK);
 					printf("\n不是你的回合");
-
 				}
 					break;
 	 	case 0:
@@ -376,464 +374,33 @@ void zouzi(int he)
 	}
 }
 
-
-//==================================================================================
-
-struct startPoints startPoints[] = {0,0}; // 用于存储每个遍历的点
-struct points_list points_list[] = { 0,0 }; // 用来存储最优选择点
-int score_list[] = {0}; // 用来存储每个电脑棋子的最优选择点的预估分
-int state_temp[6][6]={ { 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1 }, { 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0 }, { 2, 2, 2, 2, 2, 2 }, {  2, 2, 2, 2, 2, 2 } };
-int step_temp[6][6] = { { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 },
-			{ 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } };
-int flag2 = 0;
-// 搜寻所选棋子的飞行吃子走位（深度优先）
-// direction为搜索路线方向，0四个方向，1上，2下，3左，4右，type为棋子类型:1玩家\2电脑
-
-void fly_dfs2(int x, int y, int direction, int type, int flag2) {
-
-	// 四个角
-	if ((x == 0 && y == 0) || (x == 0 && y == 5) || (x == 5 && y == 0) || (x == 5 && y == 5)) {
-		return;
-	}
-
-	// 如果是敌方棋子且经过圆弧
-	if (state_temp[x][y] == 2 && flag2 == 1) {
-		step_temp[x][y] = 2;
-		return;
-
-	}
-	else if (state_temp[x][y] == 2) {
-		return;
-	}
-
-	// 如果是己方棋子
-	if (state_temp[x][y] == type && direction != 0) {
-		return;
-	}
-
-	// 起始点
-	if (direction == 0) {
-
-		// 如果是切点的情况
-		if (x == 0 && y == 1) {
-			fly_dfs2(1, 0, 4, type, 1);
-		}
-		else if (x == 0 && y == 2) {
-			fly_dfs2(2, 0, 4, type, 1);
-		}
-		else if (x == 0 && y == 3) {
-			fly_dfs2(2, 5, 3, type, 1);
-		}
-		else if (x == 0 && y == 4) {
-			fly_dfs2(1, 5, 3, type, 1);
-		}
-		else if (x == 1 && y == 0) {
-			fly_dfs2(0, 1, 2, type, 1);
-		}
-		else if (x == 2 && y == 0) {
-			fly_dfs2(0, 2, 2, type, 1);
-		}
-		else if (x == 3 && y == 0) {
-			fly_dfs2(5, 2, 1, type, 1);
-		}
-		else if (x == 4 && y == 0) {
-			fly_dfs2(5, 1, 1, type, 1);
-		}
-		else if (x == 1 && y == 5) {
-			fly_dfs2(0, 4, 2, type, 1);
-		}
-		else if (x == 2 && y == 5) {
-			fly_dfs2(0, 3, 2, type, 1);
-		}
-		else if (x == 3 && y == 5) {
-			fly_dfs2(5, 3, 1, type, 1);
-		}
-		else if (x == 4 && y == 5) {
-			fly_dfs2(5, 4, 1, type, 1);
-		}
-		else if (x == 5 && y == 1) {
-			fly_dfs2(4, 0, 4, type, 1);
-		}
-		else if (x == 5 && y == 2) {
-			fly_dfs2(3, 0, 4, type, 1);
-		}
-		else if (x == 5 && y == 3) {
-			fly_dfs2(3, 5, 3, type, 1);
-		}
-		else if (x == 5 && y == 4) {
-			fly_dfs2(4, 5, 3, type, 1);
-		}
-
-		// 上
-		if (x - 1 >= 0) {
-			fly_dfs2(x - 1, y, 1, type, flag2);
-		}
-
-		// 下
-		if (x + 1 <= 5) {
-			fly_dfs2(x + 1, y, 2, type, flag2);
-		}
-
-		// 左
-		if (y - 1 >= 0) {
-			fly_dfs2(x, y - 1, 3, type, flag2);
-		}
-
-		// 右
-		if (y + 1 <= 5) {
-			fly_dfs2(x, y + 1, 4, type, flag2);
-		}
-	}
-	else {
-
-		// 如果是切点
-		if (x == 0 && y == 1 && direction == 1) {
-			fly_dfs2(1, 0, 4, type, 1);
-		}
-		else if (x == 0 && y == 2 && direction == 1) {
-			fly_dfs2(2, 0, 4, type, 1);
-		}
-		else if (x == 0 && y == 3 && direction == 1) {
-			fly_dfs2(2, 5, 3, type, 1);
-		}
-		else if (x == 0 && y == 4 && direction == 1) {
-			fly_dfs2(1, 5, 3, type, 1);
-		}
-		else if (x == 1 && y == 0 && direction == 3) {
-			fly_dfs2(0, 1, 2, type, 1);
-		}
-		else if (x == 2 && y == 0 && direction == 3) {
-			fly_dfs2(0, 2, 2, type, 1);
-		}
-		else if (x == 3 && y == 0 && direction == 3) {
-			fly_dfs2(5, 2, 1, type, 1);
-		}
-		else if (x == 4 && y == 0 && direction == 3) {
-			fly_dfs2(5, 1, 1, type, 1);
-		}
-		else if (x == 1 && y == 5 && direction == 4) {
-			fly_dfs2(0, 4, 2, type, 1);
-		}
-		else if (x == 2 && y == 5 && direction == 4) {
-			fly_dfs2(0, 3, 2, type, 1);
-		}
-		else if (x == 3 && y == 5 && direction == 4) {
-			fly_dfs2(5, 3, 1, type, 1);
-		}
-		else if (x == 4 && y == 5 && direction == 4) {
-			fly_dfs2(5, 4, 1, type, 1);
-		}
-		else if (x == 5 && y == 1 && direction == 2) {
-			fly_dfs2(4, 0, 4, type, 1);
-		}
-		else if (x == 5 && y == 2 && direction == 2) {
-			fly_dfs2(3, 0, 4, type, 1);
-		}
-		else if (x == 5 && y == 3 && direction == 2) {
-			fly_dfs2(3, 5, 3, type, 1);
-		}
-		else if (x == 5 && y == 4 && direction == 2) {
-			fly_dfs2(4, 5, 3, type, 1);
-		}
-
-		// 上
-		if (direction == 1) {
-			if (x - 1 >= 0) {
-				fly_dfs2(x - 1, y, direction, type, flag2);
-			}
-		}
-
-		// 下
-		if (direction == 2) {
-			if (x + 1 <= 5) {
-				fly_dfs2(x + 1, y, direction, type, flag2);
-			}
-		}
-
-		// 左
-		if (direction == 3) {
-			if (y - 1 >= 0) {
-				fly_dfs2(x, y - 1, direction, type, flag2);
-			}
-		}
-
-		// 右
-		if (direction == 4) {
-			if (y + 1 <= 5) {
-				fly_dfs2(x, y + 1, direction, type, flag2);
-			}
-		}
-	}
-}
-
-
-
- //机器AI
 void AI() {
-	memset(startPoints,0,sizeof(startPoints));
-	//startPoints.clear();
-	// points_list.clear();
-	// score_list.clear();
-
-	int x = 0;
-	int y = 0;
-
-	int maxs = -127;
-	int f = 0;
-	int score = 0;
-
-	// 遍历所有的电脑棋子
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 6; j++) {
-
-
-			if (map[i][j].name == 1) {
-				for (int m = 0; m < 6; m++) {
-					for (int n = 0; n < 6; n++) {
-						state_temp[m][n] = map[m][n].name;
-					}
-				}
-
-				for (int m = 0; m < 6; m++) {
-					for (int n = 0; n < 6; n++)
-						step_temp[m][n] = 0;
-				}
-
-				search1(i, j);
-			}
-		}
+	if (!map[2][1].name) {
+		map[1][0].name = 0;
+		map[2][1].name = 1;
+		/*msgmove.nhb++;*/
 	}
-
-	for (int i = 0; i < sizeof(score_list) / sizeof(score_list[0]); i++) {
-		//System.out.println(score_list.get(i));
-		//System.out.println(startPoints.get(i).x + " " + startPoints.get(i).y);
-		//System.out.println(points_list.get(i).x + " " + points_list.get(i).y);
-		if (score_list[i] > maxs) {
-			maxs = score_list[i];
-			f = i;
-		}
+	else if(!map[1][0].name) {
+		map[0][0].name = 0;
+		map[1][0].name = 1;
+		//msgmove.nhb++;
 	}
-
-	map[points_list[f].x][points_list[f].y].name = 1;
-	map[startPoints[f].x][startPoints[f].y].name = 0;
-	printf("\n points_list x:%d,y:%d", points_list[f].x, points_list[f].y);
-	printf("  startPoints x:%d,y:%d", startPoints[f].x, startPoints[f].y); 
+	else if (!map[2][3].name) {
+		map[1][4].name = 0;
+		map[2][3].name = 1;
+		//msgmove.nhb++;
+	}
+	else if (!map[1][4].name) {
+		map[0][5].name = 0;
+		map[1][4].name = 1;
+		//msgmove.nhb++;
+	}
+	else if (!map[2][0].name) {
+		map[1][0].name = 0;
+		map[2][0].name = 1;
+		//msgmove.nhb++;
+	}
 	msgmove.nhb++;
-
-	//		System.out.println(points_list.get(f).x + " " + points_list.get(f).y);
-	//		System.out.println(startPoints.get(f).x + " " + startPoints.get(f).y);
-	//		for(int i = 0; i < 6; i++) {
-	//			for(int j = 0; j < 6; j++) {
-	//				System.out.print(myPanel.state[i][j]+" ");
-	//			}
-	//			System.out.print("\n");
-	//		}
-}
-
-// 电脑玩家对弈走法模拟，返回（X，Y）棋子的最佳着棋
-void search1(int x, int y) {
-
-	int maxs = -65536;
-	int score = 0;
-	int xx = 0, yy = 0;
-	// 回合模拟
-	setStep1(x, y, 1);
-
-
-	int s1[6][6];
-	int s2[6][6];
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 6; j++) {
-			s1[i][j] = map[i][j].name;
-		}
-	}
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 6; j++) {
-			s2[i][j] = step_temp[i][j];
-		}
-	}
-	//		
-	//		for(int i = 0;i < 6; i++) {
-	//			for(int j = 0; j < 6; j++) {
-	//				System.out.print(step_temp[i][j]+" ");
-	//			}
-	//			System.out.print("\n");
-	//		}
-
-			//遍历可落子点
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 6; j++) {
-
-			for (int a1 = 0; a1 < 6; a1++) {
-				for (int a2 = 0; a2 < 6; a2++) {
-					state_temp[a1][a2] = s1[a1][a2];
-					
-				}
-			}
-			for (int a1 = 0; a1 < 6; a1++) {
-				for (int a2 = 0; a2 < 6; a2++) {
-					step_temp[a1][a2] = s2[a1][a2];
-				}
-			}
-
-			if (step_temp[i][j] == 1) {
-
-				score = 0;
-				if (state_temp[i][j] == 2) { //吃子
-
-					score += 2;
-					state_temp[i][j] = 1;
-					state_temp[x][y] = 0;
-				}
-				else { //不吃子
-					score++;
-					state_temp[i][j] = 1;
-					state_temp[x][y] = 0;
-				}
-
-				//int[][] a = state_temp.clone();
-				int a[6][6];
-				for (int a1 = 0; a1 < 6; a1++) {
-					for (int a2 = 0; a2 < 6; a2++) {
-						a[a1][a2] = state_temp[a1][a2];
-						
-					}
-				}
-
-				//模拟玩家
-				for (int m = 0; m < 6; m++) {
-					for (int n = 0; n < 6; n++) {
-
-						//state_temp = a.clone();
-						for (int a1 = 0; a1 < 6; a1++) {
-							for (int a2 = 0; a2 < 6; a2++) {
-								state_temp[a1][a2] = a[a1][a2];
-								
-
-							}
-						}
-
-						if (state_temp[m][n] == 1) {
-
-							for (int x1 = 0; x1 < 6; x1++) {
-								for (int y1 = 0; y1 < 6; y1++) {
-									step_temp[x1][y1] = 0;
-								}
-							}
-
-							int score1 = score;
-							setStep1(m, n, 2);
-
-							//								int[][] s3 = state_temp.clone();
-							//								int[][] s4 = step_temp.clone();
-							int s3[6][6];
-							int s4[6][6];
-							for (int a1 = 0; a1 < 6; a1++) {
-								for (int a2 = 0; a2 < 6; a2++) {
-									s3[a1][a2] = state_temp[a1][a2];
-								}
-							}
-							for (int a1 = 0; a1 < 6; a1++) {
-								for (int a2 = 0; a2 < 6; a2++) {
-									s4[a1][a2] = step_temp[a1][a2];
-								}
-							}
-
-							for (int o = 0; o < 6; o++) {
-								for (int p = 0; p < 6; p++) {
-
-									//										state_temp = s3.clone();
-									//										step_temp = s4.clone();
-									for (int a1 = 0; a1 < 6; a1++) {
-										for (int a2 = 0; a2 < 6; a2++) {
-											state_temp[a1][a2] = s3[a1][a2];
-										}
-									}
-									for (int a1 = 0; a1 < 6; a1++) {
-										for (int a2 = 0; a2 < 6; a2++) {
-											step_temp[a1][a2] = s4[a1][a2];
-										}
-									}
-
-									if (step_temp[o][p] == 1) {
-										//吃子
-										if (state_temp[o][p] == 2) {
-											score1 -= 2;
-										}
-										else {
-											score1++;
-										}
-									}
-								}
-							}
-							if (score1 > score1) {
-								score = score1;
-							}
-						}
-
-					}
-				}
-				if (score > maxs) {
-					xx = i;
-					yy = j;
-					maxs = score;
-				}
-			}
-		}
-	}
-
-	int lenstartPoints = sizeof(startPoints) / sizeof(startPoints[0]);
-	int lenpoints_list = sizeof(points_list) / sizeof(points_list[0]);
-	int lenscore_list = sizeof(score_list) / sizeof(score_list[0]);
-
-	//startPoints.add(new Point(x, y));
-	startPoints[lenstartPoints] = {x,y};
-	points_list[lenpoints_list] = {xx,yy};
-	//points_list.add(new Point(xx, yy));
-	score_list[lenscore_list] = maxs;
-	printf("lenstartPoints:%d  %d\n", x,y);
-	printf("maxs:%d",maxs);
-}
-
-// ai 将某个棋子可走的点标注出来
-void setStep1(int x, int y, int type) {
-
-	if (x - 1 >= 0) {
-		if (state_temp[x - 1][y] != type)
-			step_temp[x - 1][y] = 2;
-	}
-	if (x + 1 <= 5) {
-		if (state_temp[x + 1][y] != type)
-			step_temp[x + 1][y] = 2;
-	}
-	if (y - 1 >= 0) {
-		if (state_temp[x][y - 1] != type)
-			step_temp[x][y - 1] = 2;
-	}
-	if (y + 1 <= 5) {
-		if (state_temp[x][y + 1] != type)
-			step_temp[x][y + 1] = 2;
-	}
-	if (x - 1 >= 0 && y - 1 >= 0) {
-		if (state_temp[x - 1][y - 1] != type)
-			step_temp[x - 1][y - 1] = 2;
-	}
-	if (x + 1 <= 5 && y - 1 >= 0) {
-		if (state_temp[x + 1][y - 1] != type)
-			step_temp[x + 1][y - 1] = 2;
-	}
-	if (x - 1 >= 0 && y + 1 <= 5) {
-		if (state_temp[x - 1][y + 1] != type)
-			step_temp[x - 1][y + 1] = 2;
-	}
-	if (x + 1 <= 5 && y + 1 <= 5) {
-		if (state_temp[x + 1][y + 1] != type)
-			step_temp[x + 1][y + 1] = 2;
-	}
-
-	fly_dfs2(x, y, 0, type, 0);
-
 }
 
 
